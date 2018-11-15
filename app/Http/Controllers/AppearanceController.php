3c6e9ee7154aa75a05760703c565b3d533e16917
abaume <baume.aurore@gmail.com>
2018-11-15 21:56:25 +0100

    images

--- app/Http/Controllers/AppearanceController.php
+++ app/Http/Controllers/AppearanceController.php
@@ -19,4 +19,26 @@ class AppearanceController extends Controller
     {
         return AppearanceResource::collection(Appearance::query()->where('race', $race->id)->get());
     }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request) {
+        $this->validate($request, [
+            'input_img' => 'required|image|mimes:png|max:2048',
+        ]);
+
+        if ($request->hasFile('input_img')) {
+            $image = $request->file('input_img');
+            $name = $request->input('color_name').'.'.$image->getClientOriginalExtension();
+            $destinationPath = public_path('/assets/dragons/' . $request->input('race_name') . '/');
+            $image->move($destinationPath, $name);
+            $this->save();
+
+            return back()->with('success','Image Upload successfully');
+        }
+    }
 }
